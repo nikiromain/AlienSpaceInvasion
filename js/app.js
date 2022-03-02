6d8c04082397d7db88a5cd16a570eb3c05bd8959
Nikisha Romain <nikisharomain@Nikishas-MacBook-Air.local>
2022-03-02 00:23:53 -0400

    finish collision

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 const main = (()=>
 {
 
+
     let marginPxMove = 10
     let gridColumn = 1;
     let bulletMargin = -10
@@ -12,7 +13,7 @@ const main = (()=>
     const canon = document.querySelector("#canon")
     const bullet = document.querySelector("#bullet")
 
-    console.log(spaceshipList.length)
+   
 
         setInterval(()=>
         {
@@ -21,19 +22,16 @@ const main = (()=>
                 marginPxMove += i;
                 spaceshipList[i].style.marginTop = `${marginPxMove}px`
                 
+                let canonBox =canon.getBoundingClientRect();
+                        let shipBox =   spaceshipList[i].getBoundingClientRect();
+        
+                        if(canonBox.top <= shipBox.bottom){
+                            
+                           alert("Game over")
+                        } 
             }
-/*
-            const spaceshipRect = spaceContainers.getBoundingClientRect()
-            marginPxMove +=5
-            console.log(spaceshipRect.top)
-
-            spaceContainers.style.marginTop = `${marginPxMove}px`
-
-
-            marginPxMove += 10;
-            spaceshipList[0].style.marginTop = `${marginPxMove}px`
-            console.log("It working")
-            */
+            
+            
         }, 1000)
         
 
@@ -58,14 +56,36 @@ const main = (()=>
             {
                 bullet.style.display = "block";
                 
-                setInterval(() => {
+               const detectCollision = setInterval(() => {
                     bullet.style.marginTop = `${bulletMargin}px `
-                    bulletMargin += -10
-                }, 500)
+                    bulletMargin += -20
+
+                   
+                    for (let i = 0; i < spaceshipList.length; i++) {
+                      
+                        
+                        let bulletBox = bullet.getBoundingClientRect();
+                        let shipBox =   spaceshipList[i].getBoundingClientRect();
+        
+                        if(bulletBox.top <= shipBox.bottom){
+                            
+                            spaceshipList[i].style.marginTop = 0
+                            marginPxMove = 10;
+                            bulletMargin = -10
+                            bullet.style.display = "none"
+                            bullet.style.marginTop = 0
+                            clearInterval(detectCollision)
+                        } 
+                    }
+
+                    
+                }, 300)
             }
 
          })
         
+      
+      
         
     
 
